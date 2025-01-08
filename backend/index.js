24ff3ce4ee1202bb8efded0af6cea967ab8a0681
Gpooja13 <145600866+Gpooja13@users.noreply.github.com>
2025-01-09 00:34:07 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ connectDB();
 
 app.use("/api/auth", AuthRoutes);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend/build", "index.html")),
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html")),
     function (err) {
       res.status(500).send(err);
     };
